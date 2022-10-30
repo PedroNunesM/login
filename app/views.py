97c9c900464523a810dd83893f398483f92bfe3e
PedroNunesM <pekfnd12@gmail.com>
2022-10-30 18:40:10 -0300

    add

--- app/views.py
+++ app/views.py
@@ -5,11 +5,14 @@ from django.shortcuts import render
 def index(request):
     data = {}
     data['login'] = False
+    data['tentativa'] = False
     if request.method == 'POST':
         if request.POST['login'] == 'admin' and request.POST['password'] == 'teste':
             data['login'] = True
+            data['tentativa'] = False
             return render(request, 'app/index.html', data, status=200)
         else:
             data['login'] = False
+            data['tentativa'] = True
             return render(request, 'app/index.html', data, status=400)
     return render(request, 'app/index.html', data)
